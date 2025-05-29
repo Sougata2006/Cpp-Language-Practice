2cc8fd4c39cf279eaa89f4e9586ac6354ded64a4
Sougata Paul <psougata32@gmail.com>
2025-05-29 19:56:23 +0530

    Create 4.cpp

---
+++ 4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// if i don't use std i have to right here that 
+// using namespace std;
+
+int main (){
+int a = 0 , b = 7 ;
+float oi = 4.56;
+std :: cout << " this is 4.c . and the value of a is "<<a<<"\n and the value of b is "<<b<<".\n";
+std:: cout << " the value of float is :"<<oi;
+
+
+
+    return 0 ;
+}
