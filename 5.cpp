e4ec24b266a181ddc509c2b3e81e6f237a1d2f7e
Sougata Paul <psougata32@gmail.com>
2025-05-30 13:16:11 +0530

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+//after c language there are a new datatype called string ---
+// although there are int , char , float datatype also ---
+// we can use verious data type for print integers -  short<int<long<long long ---
+float const num = 0.265;
+// num = 0.32; it will be a error but if i don't use const then it will change the float
+
+cout <<"the num is "<< num;
+    return 0;
+}
