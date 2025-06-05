7d1fa6f6d1b8a75995b618c9a154336cf91c9afb
Sougata Paul <psougata32@gmail.com>
2025-06-05 22:40:19 +0530

    Create 11.cpp

---
+++ 11.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main (){
+//arrayss--------------
+int arr [3]= {12,13,14};
+//array index 0  1  2 
+cout<<arr[2]<<"\n";
+cout<<arr[0]<<"\n";
+cout<<arr[1]<<"\n";
+
+
+    return 0;
+}
