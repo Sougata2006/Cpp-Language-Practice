74caa1aad60049646dae12d95aa31585dadc26e1
Sougata Paul <psougata32@gmail.com>
2025-06-20 22:52:15 +0530

    Create 17.cpp

---
+++ 17.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std ;
+
+class employe {
+public :
+string name ;
+int salary ;
+//00
+};
+
+int main (){
+
+employe har ;
+har.name = "sougata";
+har.salary = 100;
+cout<<"the name of our first employe is "<<har.name<<"\n"<<"ans his salary is "<<har.salary<<"\n";
+
+    return 0;
+}
